232250448a6630281c6ff563854773b758b69321
BruceYangOODA <lucifelex@hotmail.com>
2019-11-08 12:45:48 +0800

    HelloWorld

---
+++ app/src/main/java/com/not_a_name/helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.not_a_name.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
